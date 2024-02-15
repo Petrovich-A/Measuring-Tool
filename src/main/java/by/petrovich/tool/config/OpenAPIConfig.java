089cfe659826558ef5d2ce1a38feb3c1feabbab4
Petrovich Alexandr <A.Piatrovich@gmail.com>
2024-02-15 13:28:36 +0300

    moved to constants

--- src/main/java/by/petrovich/tool/config/OpenAPIConfig.java
+++ src/main/java/by/petrovich/tool/config/OpenAPIConfig.java
@@ -13,6 +13,23 @@ import java.util.List;
 
 @Configuration
 public class OpenAPIConfig {
+    public static final String SERVER_URL_IN_DEVELOPMENT_ENVIRONMENT = "Server URL in Development environment";
+    public static final String SERVER_URL_IN_PRODUCTION_ENVIRONMENT = "Server URL in Production environment";
+    public static final String EMAIL = "a.piatrovich@gmail.com";
+    public static final String NAME = "Alexandr Petrovich";
+    public static final String URL = "https://www.measuring-tool.com";
+    public static final String LICENSE_NAME = "MIT License";
+    public static final String LICENSE_URL = "https://";
+    public static final String TITLE = "Measuring-Tool API";
+    public static final String VERSION = "1.0";
+    public static final String DESCRIPTION = "The Instrument Control System is a Java-based project aimed at managing and tracking measurement" +
+            " tools in warehouse facilities at a large-scale engineering enterprise. Upon registration and login to the portal," +
+            " the warehouse manager gains the ability to register measurement tools based on their type, lab number, and other " +
+            "parameters. Furthermore, the manager can issue tools to enterprise employees, registering the issuance via " +
+            "the application. Additionally, the system is designed to send notifications to employees requiring them to submit " +
+            "the tools for inspection a specific time before the scheduled verification date, ensuring timely maintenance and " +
+            "quality control of the instruments.";
+    public static final String TERMS_URL = "https://www.measuring-tool.com/terms";
     @Value("${petrovich.openapi.dev-url}")
     private String devUrl;
 
@@ -23,31 +40,25 @@ public class OpenAPIConfig {
     public OpenAPI toolOpenAPI() {
         Server devServer = new Server();
         devServer.setUrl(devUrl);
-        devServer.setDescription("Server URL in Development environment");
+        devServer.setDescription(SERVER_URL_IN_DEVELOPMENT_ENVIRONMENT);
 
         Server prodServer = new Server();
         prodServer.setUrl(prodUrl);
-        prodServer.setDescription("Server URL in Production environment");
+        prodServer.setDescription(SERVER_URL_IN_PRODUCTION_ENVIRONMENT);
 
         Contact contact = new Contact();
-        contact.setEmail("a.piatrovich@gmail.com");
-        contact.setName("Alexandr Petrovich");
-        contact.setUrl("https://www.measuring-tool.com");
+        contact.setEmail(EMAIL);
+        contact.setName(NAME);
+        contact.setUrl(URL);
 
-        License mitLicense = new License().name("MIT License").url("https://");
+        License mitLicense = new License().name(LICENSE_NAME).url(LICENSE_URL);
 
         Info info = new Info()
-                .title("Measuring-Tool API")
-                .version("1.0")
+                .title(TITLE)
+                .version(VERSION)
                 .contact(contact)
-                .description("The Instrument Control System is a Java-based project aimed at managing and tracking measurement" +
-                        " tools in warehouse facilities at a large-scale engineering enterprise. Upon registration and login to the portal," +
-                        " the warehouse manager gains the ability to register measurement tools based on their type, lab number, and other " +
-                        "parameters. Furthermore, the manager can issue tools to enterprise employees, registering the issuance via " +
-                        "the application. Additionally, the system is designed to send notifications to employees requiring them to submit " +
-                        "the tools for inspection a specific time before the scheduled verification date, ensuring timely maintenance and " +
-                        "quality control of the instruments.")
-                .termsOfService("https://www.measuring-tool.com/terms")
+                .description(DESCRIPTION)
+                .termsOfService(TERMS_URL)
                 .license(mitLicense);
 
         return new OpenAPI().info(info).servers(List.of(devServer, prodServer));
